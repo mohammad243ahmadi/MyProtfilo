b6c2c115b36e69f443f6d1c0d47ed38d6071ae20
mohammad243ahmadi <mohammad243ahmadi@gmail.com>
2026-03-09 09:12:18 +0430

    link to the safir project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,7 +49,7 @@ const projects = [
             "A modern educational platform with a proper client/server architecture.",
         technologies: ["React", "Node.js", "Express", "PostgreSQL"],
         github: "#",
-        live: "#",
+        live: "https://safir-foundation.org",
         image: "https://plus.unsplash.com/premium_vector-1720082660197-b1a163e1372a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Q291cnNlfGVufDB8fDB8fHww",
     },
 ];
